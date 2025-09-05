bb07f1f15c9248b9da4e2babb18e6e62481e47f3
shibainu0001 <huminokahomoe@yahoo.co.jp>
2025-09-05 14:54:26 +0900

    para fix

--- run_all.sh
+++ run_all.sh
@@ -2,4 +2,10 @@ FUND_ID="0331418A"
 IMAGE_DIR="/data/data/com.termux/files/home/storage/documents/stock/plot_images"
 python update.py $FUND_ID > /dev/null
 # python bandwalk.py $FUND_ID $IMAGE_DIR "ALL"
-python bandwalk_core_impl.py $FUND_ID DUMMY_DIR "ALL" 85.7 -41.5 0.815 0.846
+
+UPPER_THRESHOLD=52.99
+UPPER_CROSS_RATE=0.952
+LOWER_THRESHOLD=-48.92
+LOWER_CROSS_RATE=0.999
+
+python bandwalk_core_impl.py $FUND_ID DUMMY_DIR "ALL" $UPPER_THRESHOLD $LOWER_THRESHOLD $UPPER_CROSS_RATE $LOWER_CROSS_RATE
--- run_atmx.sh
+++ run_atmx.sh
@@ -2,4 +2,10 @@ FUND_ID="04315213"
 IMAGE_DIR="/data/data/com.termux/files/home/storage/documents/stock/plot_images"
 python update.py $FUND_ID > /dev/null
 # python bandwalk.py $FUND_ID $IMAGE_DIR ATMX+
-python bandwalk_core_impl.py $FUND_ID DUMMY_DIR ATMX+ 180.7 -4.762 0.897 0.603
+
+UPPER_THRESHOLD=180.94
+UPPER_CROSS_RATE=0.82
+LOWER_THRESHOLD=-4.65
+LOWER_CROSS_RATE=0.619
+
+python bandwalk_core_impl.py $FUND_ID DUMMY_DIR ATMX+ $UPPER_THRESHOLD $LOWER_THRESHOLD $UPPER_CROSS_RATE $LOWER_CROSS_RATE
